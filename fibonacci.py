c4d15052592587225fa9a4ea8f8c62cf7fb7cc7a
Eloxyd3s <112265562+Eloxyd3s@users.noreply.github.com>
2025-09-01 15:17:47 +0200

    makeover
    re thought it with my knowledge and what i can do with an input of the user

--- fibonacci.py
+++ fibonacci.py
@@ -1,1 +1,12 @@
-def fibonacci:
+base = "0 1 "
+a, b = 0, 1
+data = 0
+n = int(input("amount of numbers?"))
+while data <= n:
+    c = a + b
+    base += str(c)+" "
+    data += 1
+    a = b
+    b = c
+
+print(base)
